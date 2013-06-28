4210af3719466d08c2db97ec64662482f93d68c8
THIAGO PEREIRA <thiperei@sayyadina01.known-universe.com>
2013-06-28 15:56:20 -0700

    Initial Commit

---
+++ MapKitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapKitTest
+//
+//  Created by THIAGO PEREIRA on 6/28/13.
+//  Copyright (c) 2013 THIAGO PEREIRA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapKitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapKitTest
+//
+//  Created by THIAGO PEREIRA on 6/28/13.
+//  Copyright (c) 2013 THIAGO PEREIRA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
